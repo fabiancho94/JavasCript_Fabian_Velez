f4f18e8c5ba347b5620eb8e945687ca89a2cf77a
Fabian Velez <fabianncho94@gmail.com>
2018-01-22 12:56:05 -0500

    Terminando animaciones

--- js/app.js
+++ js/app.js
@@ -17,13 +17,13 @@ var calculadora = {
     var signo = document.getElementById('sign')
     var punto = document.getElementById('punto')
     var dividido = document.getElementById('dividido')
-    var tecla1 = document.getElementById('menos')
-    var tecla1 = document.getElementById('mas')
-    var tecla1 = document.getElementById('por')
+    var menos = document.getElementById('menos')
+    var mas = document.getElementById('mas')
+    var por = document.getElementById('por')
     var igual = document.getElementById('igual')
     /* Animacion de teclas*/
     tecla0.addEventListener("click", function(){
-      
+
     })
     tecla0.addEventListener("mousedown", function(){
       tecla0.setAttribute("style", "transform: scale(0.95,0.95)")
@@ -31,5 +31,161 @@ var calculadora = {
     tecla0.addEventListener("mouseout", function(){
       tecla0.setAttribute("style", "transform: scale(1,1)")
     })
+    tecla1.addEventListener("click", function(){
+
+    })
+    tecla1.addEventListener("mousedown", function(){
+      tecla1.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla1.addEventListener("mouseout", function(){
+      tecla1.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla2.addEventListener("click", function(){
+
+    })
+    tecla2.addEventListener("mousedown", function(){
+      tecla2.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla2.addEventListener("mouseout", function(){
+      tecla2.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla3.addEventListener("click", function(){
+
+    })
+    tecla3.addEventListener("mousedown", function(){
+      tecla3.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla3.addEventListener("mouseout", function(){
+      tecla3.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla4.addEventListener("click", function(){
+
+    })
+    tecla4.addEventListener("mousedown", function(){
+      tecla4.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla4.addEventListener("mouseout", function(){
+      tecla4.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla5.addEventListener("click", function(){
+
+    })
+    tecla5.addEventListener("mousedown", function(){
+      tecla5.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla5.addEventListener("mouseout", function(){
+      tecla5.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla6.addEventListener("click", function(){
+
+    })
+    tecla6.addEventListener("mousedown", function(){
+      tecla6.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla6.addEventListener("mouseout", function(){
+      tecla6.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla7.addEventListener("click", function(){
+
+    })
+    tecla7.addEventListener("mousedown", function(){
+      tecla7.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla7.addEventListener("mouseout", function(){
+      tecla7.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla8.addEventListener("click", function(){
+
+    })
+    tecla8.addEventListener("mousedown", function(){
+      tecla8.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla8.addEventListener("mouseout", function(){
+      tecla8.setAttribute("style", "transform: scale(1,1)")
+    })
+    tecla9.addEventListener("click", function(){
+
+    })
+    tecla9.addEventListener("mousedown", function(){
+      tecla9.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    tecla9.addEventListener("mouseout", function(){
+      tecla9.setAttribute("style", "transform: scale(1,1)")
+    })
+    on.addEventListener("click", function(){
+
+    })
+    on.addEventListener("mousedown", function(){
+      on.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    on.addEventListener("mouseout", function(){
+      on.setAttribute("style", "transform: scale(1,1)")
+    })
+    signo.addEventListener("click", function(){
+
+    })
+    signo.addEventListener("mousedown", function(){
+      signo.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    signo.addEventListener("mouseout", function(){
+      signo.setAttribute("style", "transform: scale(1,1)")
+    })
+    punto.addEventListener("click", function(){
+
+    })
+    punto.addEventListener("mousedown", function(){
+      punto.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    punto.addEventListener("mouseout", function(){
+      punto.setAttribute("style", "transform: scale(1,1)")
+    })
+    mas.addEventListener("click", function(){
+
+    })
+    mas.addEventListener("mousedown", function(){
+      mas.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    mas.addEventListener("mouseout", function(){
+      mas.setAttribute("style", "transform: scale(1,1)")
+    })
+    menos.addEventListener("click", function(){
+
+    })
+    menos.addEventListener("mousedown", function(){
+      menos.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    menos.addEventListener("mouseout", function(){
+      menos.setAttribute("style", "transform: scale(1,1)")
+    })
+    por.addEventListener("click", function(){
+
+    })
+    por.addEventListener("mousedown", function(){
+      por.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    por.addEventListener("mouseout", function(){
+      por.setAttribute("style", "transform: scale(1,1)")
+    })
+    dividido.addEventListener("click", function(){
+
+    })
+    dividido.addEventListener("mousedown", function(){
+      dividido.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    dividido.addEventListener("mouseout", function(){
+      dividido.setAttribute("style", "transform: scale(1,1)")
+    })
+    igual.addEventListener("click", function(){
+
+    })
+    igual.addEventListener("mousedown", function(){
+      igual.setAttribute("style", "transform: scale(0.95,0.95)")
+    })
+    igual.addEventListener("mouseout", function(){
+      igual.setAttribute("style", "transform: scale(1,1)")
+    })
+
   }
 }
+
+calculadora.init()
